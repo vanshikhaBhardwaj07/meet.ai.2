22d06f0eecc58efe164966bfcb79c0f1a587d50a
Vanshika Bhardwaj <bhardwajvanshika085@gmail.com>
2025-09-25 12:16:37 -0700

    10:RESPONSIVE-DIALOG

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -8,7 +8,10 @@ export const AgentsView = () => {
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
 
-  return <div>{JSON.stringify(data, null, 2)}</div>;
+  return(
+     <div>{JSON.stringify(data, null, 2)}</div>
+    )
+
 };
 export const AgentsViewLoading = () => {
     return (
